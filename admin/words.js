9cafe34512bdb6556b0e2fc351629b0d547e9852
Lutz Lengemann <lutz@lengemann.net>
2020-01-03 22:56:18 +0100

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'freeathome adapter settings': {
+        'en': 'Adapter settings for freeathome',
+        'de': 'Adaptereinstellungen für freeathome',
+        'ru': 'Настройки адаптера для freeathome',
+        'pt': 'Configurações do adaptador para freeathome',
+        'nl': 'Adapterinstellingen voor freeathome',
+        'fr': "Paramètres d'adaptateur pour freeathome",
+        'it': "Impostazioni dell'adattatore per freeathome",
+        'es': 'Ajustes del adaptador para freeathome',
+        'pl': 'Ustawienia adaptera dla freeathome',
+        'zh-cn': 'freeathome的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
